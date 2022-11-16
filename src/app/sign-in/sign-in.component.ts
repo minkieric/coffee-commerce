896e9e889578f7aa08c87021f1e33cff6e159905
Eric Lee <minkiericlee@gmail.com>
2022-11-15 19:31:44 -0800

    second

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor() { }
+  signInForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
 
-  ngOnInit(): void {
-  }
-
+  signUpForm = this.formBuilder.group({
+    lastName: ['', Validators.required],
+    firstName: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+    address: this.formBuilder.group({
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      zipcode: ['', Validators.required],
+    }),
+  });
+  ngOnInit(): void {}
 }
